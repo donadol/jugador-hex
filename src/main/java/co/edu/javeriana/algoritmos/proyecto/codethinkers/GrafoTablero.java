0b4e8c1cc6915efb16f5d6015509ecba46a40def
Jhonny <jhonny_parra@javeriana.edu.co>
2019-05-28 18:50:52 -0500

    Ya se mide la distancia entre los bordes del tablero

---
+++ src/main/java/co/edu/javeriana/algoritmos/proyecto/codethinkers/GrafoTablero.java
@@ -0,0 +1,300 @@
+package co.edu.javeriana.algoritmos.proyecto.codethinkers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+import co.edu.javeriana.algoritmos.proyecto.ColorJugador;
+
+public class GrafoTablero {
+	private static int INF = Integer.MAX_VALUE;
+	
+	private static int TAM_TABLERO = 11;
+	
+	private static int BORDE_BLANCO_IZQUIERDO = -1;
+	private static int BORDE_BLANCO_DERECHO = 11;
+	
+	private static int BORDE_NEGRO_SUPERIOR = -1;
+	private static int BORDE_NEGRO_INFERIOR = 11;
+	
+	private VerticeHex [][] tablero;
+	
+	
+	private VerticeHex bordeNegroSuperior; 
+	private VerticeHex bordeNegroInferior;
+	private VerticeHex bordeBlancoIzquierda;
+	private VerticeHex bordeBlancoDerecha;
+
+	public GrafoTablero() {
+		// TODO Auto-generated constructor stub
+		//el tablero es representado como una matriz de 11 x 11
+		
+		tablero = new VerticeHex [11][11];
+		for (int i = 0; i < 11; i++) {
+			for (int j = 0; j < 11; j++) {
+				tablero[i][j] = new VerticeHex();
+				tablero[i][j].setFila(i);
+				tablero[i][j].setColumna(j);
+			}
+		}
+		
+		bordeNegroSuperior = new VerticeHex(ColorJugador.NEGRO);
+		bordeNegroSuperior.setFila(BORDE_NEGRO_SUPERIOR);
+		
+		bordeNegroInferior = new VerticeHex(ColorJugador.NEGRO); 
+		bordeNegroInferior.setFila(BORDE_NEGRO_INFERIOR);
+		
+		bordeBlancoIzquierda = new VerticeHex(ColorJugador.BLANCO);
+		bordeBlancoIzquierda.setColumna(BORDE_BLANCO_IZQUIERDO);
+		
+		bordeBlancoDerecha = new VerticeHex(ColorJugador.BLANCO);
+		bordeBlancoDerecha.setColumna(BORDE_BLANCO_DERECHO);
+		
+		
+		
+	}
+	
+	
+	
+	
+	public int obtenerDistanciaBordes (ColorJugador color) {
+		VerticeHex v;
+		int costoAristavu;
+		//Reinicia los valores de distancia de cada uno de los vertices
+		reiniciarDistancias();
+		reiniciarMarcas();
+
+
+		VerticeHex inicio;
+		if (color == ColorJugador.BLANCO) 
+			inicio = bordeBlancoIzquierda;
+		else 
+			inicio = bordeNegroSuperior;
+		
+		
+		inicio.setDistancia(0);		
+		inicio.setMarcado(true);	
+
+		PriorityQueue<VerticeHex> colaPrioridad = new PriorityQueue<VerticeHex>((11*11+4), new ComparadorVerticeHex());
+		colaPrioridad.add(inicio);
+
+		while (!colaPrioridad.isEmpty()) {
+		
+			v = colaPrioridad.remove();
+			v.setMarcado(true);
+			//System.out.println("Bueno: "+ v.getFila() +"  "+v.getColumna());
+			for (VerticeHex u: obtenerVecinos(v, color)) {
+				//System.out.println("	Vecino: " + u.getFila() +" "+ u.getColumna());
+				if (!u.isMarcado()) {
+					costoAristavu = obtenerCostoArista(v, u, color);
+					if (u.getDistancia() > v.getDistancia() + costoAristavu) {
+						u.setDistancia(v.getDistancia() + costoAristavu);
+						colaPrioridad.offer(u);
+					}
+				}
+			}
+		}
+		
+		if (color == ColorJugador.BLANCO) 
+			return bordeBlancoDerecha.getDistancia() - 1;
+		else
+			return bordeNegroInferior.getDistancia() - 1;
+		
+	}
+	
+	private void reiniciarDistancias () {
+		for (int i = 0; i < 11 ; i++) {
+			for (int j = 0; j < 11; j++) {
+				tablero [i][j].setDistancia(INF);
+			}
+		}
+		
+		bordeNegroSuperior.setDistancia(INF);
+		bordeNegroInferior.setDistancia(INF);
+		bordeBlancoIzquierda.setDistancia(INF);
+		bordeBlancoDerecha.setDistancia(INF);
+	}
+	
+	private void reiniciarMarcas () {
+		
+		for (int i = 0; i < 11 ; i++) {
+			for (int j = 0; j < 11; j++) {
+				tablero [i][j].setMarcado(false);
+			}
+		}
+		
+		bordeNegroSuperior.setMarcado(false);
+		bordeNegroInferior.setMarcado(false);
+		bordeBlancoIzquierda.setMarcado(false);
+		bordeBlancoDerecha.setMarcado(false);
+	}
+	
+	
+	
+	
+	//OPTIMIZACIÓN MEMORIA: Crear lista como atributo y hacer clear? - menos memoria?
+	//OPTIMIZACIÓN : RETORNAR ANTES
+	List<VerticeHex> obtenerVecinos (VerticeHex v, ColorJugador color){
+		List<VerticeHex> vecinos = new ArrayList<VerticeHex>();
+		
+		
+		//Se retorna la primera columna
+		if (v.getColumna() == BORDE_BLANCO_IZQUIERDO && color == ColorJugador.BLANCO) { 
+			for (int i = 0; i < 11; i++) 
+				vecinos.add(tablero[i][0]);
+			return vecinos;
+		}
+
+		//Se retorna la última columna
+		if (v.getColumna() == BORDE_BLANCO_DERECHO && color == ColorJugador.BLANCO) { 
+			for (int i = 0; i < 11; i++) 
+				vecinos.add(tablero[i][10]);
+			return vecinos;
+		}
+			
+		
+		//Se retorna la primera fila
+		if (v.getFila() == BORDE_NEGRO_SUPERIOR && color == ColorJugador.NEGRO) {
+			for (int j = 0; j < 11; j++)
+				vecinos.add(tablero[0][j]);
+			return vecinos;
+		}
+		
+		//Se retorna la última fila
+		if (v.getFila() == BORDE_NEGRO_INFERIOR && color == ColorJugador.NEGRO) {
+			for (int j = 0; j < 11; j++)
+				vecinos.add(tablero[10][j]);
+			return vecinos;
+		}
+		
+		
+		
+		
+		int i = v.getFila();
+		int j = v.getColumna();
+		
+		//Se agregan los diferentes vecinos:
+		if (esCasillaValida(i - 1, j)) 
+			vecinos.add(tablero [i - 1][j]);
+		
+		
+		if (esCasillaValida(i + 1, j)) 
+			vecinos.add(tablero [i + 1][j]);
+				
+		if (esCasillaValida(i, j + 1)) 
+			vecinos.add(tablero [i][j + 1]);
+		
+		if (esCasillaValida(i, j - 1)) 
+			vecinos.add(tablero [i][j - 1]);
+		
+		if (esCasillaValida(i - 1, j + 1)) 
+			vecinos.add(tablero [i - 1][j + 1]);
+		
+		if (esCasillaValida(i + 1, j - 1)) 
+			vecinos.add(tablero [i + 1][j - 1]);
+		
+		
+		
+		
+		//Agregar vecino de borde
+		
+		// borde superior
+		if (i == 0 && color == ColorJugador.NEGRO)
+			vecinos.add(bordeNegroSuperior);
+		
+		// borde inferior
+		if (i == 10 && color == ColorJugador.NEGRO) 
+			vecinos.add(bordeNegroInferior);
+		
+		// borde izquierdo
+		if (j == 0 && color == ColorJugador.BLANCO)
+			vecinos.add(bordeBlancoIzquierda);
+		
+		// borde derecho
+		if (j == 10 && color == ColorJugador.BLANCO)
+			vecinos.add(bordeBlancoDerecha);		
+		
+		return vecinos;
+	}
+	
+	
+	//(si es borde el costo debería ser 0 o restar 2)	
+	private int obtenerCostoArista (VerticeHex u, VerticeHex v, ColorJugador color) {
+		
+		
+		if (u.getColor() == null && v.getColor() == null) {
+			return 1;
+		}
+		
+		if (u.getColor() == ColorJugador.BLANCO && v.getColor() == ColorJugador.NEGRO)
+			return INF;
+		
+		if (u.getColor() == ColorJugador.NEGRO && v.getColor() == ColorJugador.BLANCO)
+			return INF;
+		
+		
+		//Perspectiva del blanco
+		if (color == ColorJugador.BLANCO) {
+			
+			if (u.getColor() == ColorJugador.BLANCO && v.getColor() == ColorJugador.BLANCO)
+				return 0;
+			
+			if (u.getColor() == ColorJugador.BLANCO && v.getColor() == null)
+				return 1;
+			
+			if (u.getColor() == null && v.getColor() == ColorJugador.BLANCO)
+				return 0;
+			
+			if (u.getColor() == null && v.getColor() == ColorJugador.NEGRO)
+				return INF;
+			
+			if (u.getColor() == ColorJugador.NEGRO && v.getColor() == null)
+				return INF;
+			
+			
+						
+			
+		}
+		else { //Perspectiva del negro
+			
+			if (u.getColor() == ColorJugador.NEGRO && v.getColor() == ColorJugador.NEGRO)
+				return 0;
+			
+			if (u.getColor() == ColorJugador.NEGRO && v.getColor() == null)
+				return 1;
+			
+			if (u.getColor() == null && v.getColor() == ColorJugador.NEGRO)
+				return 0;
+			
+			if (u.getColor() == null && v.getColor() == ColorJugador.BLANCO)
+				return INF;
+			
+			if (u.getColor() == ColorJugador.BLANCO && v.getColor() == null)
+				return INF;
+		}
+		
+		return INF;			
+		
+	}
+	
+	private boolean esCasillaValida (int fila, int columna) {
+		return (   (fila >= 0 && fila < TAM_TABLERO)  &&  (columna >= 0 && columna < TAM_TABLERO) );
+	}
+	
+	
+	
+	//que pasa si la jugada está por fuera del tablero?? 
+	public void simularJugada (int fila, int columna, ColorJugador color) {
+		tablero[fila][columna].setColor(color);
+	}
+	
+	public void eliminarSimulacion (int fila, int columna) {
+		tablero[fila][columna].setColor(null);
+	}
+	
+	
+
+
+}
---
+++ src/main/java/co/edu/javeriana/algoritmos/proyecto/codethinkers/PRUEBAS.java
@@ -0,0 +1,31 @@
+package co.edu.javeriana.algoritmos.proyecto.codethinkers;
+
+import co.edu.javeriana.algoritmos.proyecto.ColorJugador;
+
+public class PRUEBAS {
+
+	public PRUEBAS() {
+		// TODO Auto-generated constructor stub
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GrafoTablero grafo = new GrafoTablero();
+		long startTime = System.nanoTime();
+		for (int i = 0; i< 40000; i++) {
+			grafo.simularJugada(5, 5, ColorJugador.BLANCO);
+			grafo.simularJugada(5, 6, ColorJugador.BLANCO);
+			System.out.println(grafo.obtenerDistanciaBordes(ColorJugador.BLANCO));
+			
+			//grafo.obtenerDistanciaBordes(ColorJugador.NEGRO);
+		}
+		long endTime = System.nanoTime();
+		System.out.println("Duración: " + (endTime-startTime)/1e6 + " ms");
+		
+	}
+	
+
+
+}
