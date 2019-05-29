01b3ade424c9f0097450f6e812b841dc019cce01
Laura Donado <ldonado11@gmail.com>
2019-05-29 17:57:12 -0500

    aleatorio

--- src/main/java/co/edu/javeriana/algoritmos/proyecto/codethinkers/LAURANOQUIEREPROBAR.java
+++ src/main/java/co/edu/javeriana/algoritmos/proyecto/codethinkers/LAURANOQUIEREPROBAR.java
@@ -13,21 +13,28 @@ public class LAURANOQUIEREPROBAR {
 		Jugada jug;
 		ColorJugador cj1 =ColorJugador.NEGRO, cj2 =ColorJugador.BLANCO;
 		int f, c;
-		for(int i=0; i<11; ++i) {
+		while(tablero.ganador()==null) {
 			do {
 				f=(int) (Math.random()*11);
 				c=(int) (Math.random()*11);
 			}while(tablero.casilla(f, c)!=null);
 			tablero.aplicarJugada(new Jugada(f,c), cj1);
+			System.out.println("aleatorio "+cj1+" "+ new Jugada(f,c));
 			jug=j1.jugar(tablero, cj2);
-			System.out.println(jug);
+			System.out.println(cj2+" "+jug);
 			tablero.aplicarJugada(jug, cj2);
 			if(jug.isCambioColores()) {
 				cj1=ColorJugador.BLANCO;
 				cj2=ColorJugador.NEGRO;
 			}
+			((TableroCodeThinkers)tablero).imprimirTablero();
 		}
-		((TableroCodeThinkers)tablero).imprimirTablero();
+		
+		if( cj2 == tablero.ganador())
+			System.out.println(tablero.ganador()+" GANA");
+		else
+			System.out.println(tablero.ganador()+" :(");
+		
 		for(int i=0; i<11; ++i) {
 			for(int j=0; j<11; ++j) {
 				System.out.print(((JugadorCodeThinkers)j1).getGrafoTablero().getTablero()[i][j].getColor()+ " ");
